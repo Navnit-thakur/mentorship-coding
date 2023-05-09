b03c45888c482cbc43d611099f7b098fd612fee1
Navnit-thakur <132601160+Navnit-thakur@users.noreply.github.com>
2023-05-08 23:30:56 -0700

    Create percentage.cpp

---
+++ percentage.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int per,a,b,c,d,e,sum=0;
+cout<<"inter no";
+cin >>a>>b>>c>>d>>e;
+sum=a+b+c+d+e;
+per=sum/10;
+cout<<per<<endl;
+return 0;
+}
