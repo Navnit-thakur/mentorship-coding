f36c9b01cf08da67991155b503ce6b7fd3e1e835
Navnit-thakur <132601160+Navnit-thakur@users.noreply.github.com>
2023-05-19 14:53:16 +0530

    Update si.cpp

--- si.cpp
+++ si.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <math>
 using namespace std;
 int main()
 {
-float p,r,t;
-float si;
+float p,r,t,n;
+float si,cm;
 cout<<"enter no";
-cin >>p>>r>>t;
+cin >>p>>r>>t>>n;
 si=(p*r*t)/100;
+cm= p*pow((1+r/n),t);
 cout<<si<<endl;
 return 0;
 }
